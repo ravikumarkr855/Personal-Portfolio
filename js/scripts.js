641e49defc9ae13ce8fbc229dca88f198a7b1f18
Ravi Kumar <ravikumarkr855@gmail.com>
2022-05-22 18:15:38 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+/* ======================= Typing Animation =========================== */
+
+var typed = new Typed(".typing", {
+    strings:[" ","Web Designer","web Developer", "Graphic Designer", "Youtuber"],
+    typeSpeed:100,
+    backSpeed:30,
+    loop:true
+})
